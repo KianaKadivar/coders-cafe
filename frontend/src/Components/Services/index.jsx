517d6da9597dff5c1a507077f87198bb541a72be
kiana.kad <kianakadivar05@gmail.com>
2025-09-03 16:35:47 +0330

    feat(services): add animations to header section
    - Integrated Framer Motion animations into Services header.
    - Added smooth entrance effects for headings and layout elements.

--- frontend/src/Components/Services/index.jsx
+++ frontend/src/Components/Services/index.jsx
@@ -1,18 +1,39 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Services() {
   return (
     <div className="container my-16 space-y-4">
       {/* header section */}
       <div className="text-center max-w-lg mx-auto space-y-2">
-        <h1 className="text-3xl font-bold text-light-gray">
+        <motion.h1
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 10,
+            delay: 0.2,
+          }}
+          className="text-3xl font-bold text-light-gray"
+        >
           Fresh and
           <span className="text-primary"> Tasty</span>
-        </h1>
-        <p className="text-sm opacity-50">
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, scale: .5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 10,
+            delay: .6,
+          }}
+          className="text-sm opacity-50"
+        >
           Freshly brewed or perfectly chilled — every sip is crafted to delight.
           From bold coffee to smooth chocolate, taste meets style in every drop.
-        </p>
+        </motion.p>
       </div>
     </div>
   );
