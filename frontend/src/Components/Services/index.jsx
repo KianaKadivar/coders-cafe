f6d28cf7c7ed5abc0b9025b6a0bdcc91d605d1a4
kiana.kad <kianakadivar05@gmail.com>
2025-09-03 16:30:39 +0330

    feat(services): add header layout structure
    - Created layout for Services header section.
    - Prepared base for adding service cards in next step.

---
+++ frontend/src/Components/Services/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div className="container my-16 space-y-4">
+      {/* header section */}
+      <div className="text-center max-w-lg mx-auto space-y-2">
+        <h1 className="text-3xl font-bold text-light-gray">
+          Fresh and
+          <span className="text-primary"> Tasty</span>
+        </h1>
+        <p className="text-sm opacity-50">
+          Freshly brewed or perfectly chilled — every sip is crafted to delight.
+          From bold coffee to smooth chocolate, taste meets style in every drop.
+        </p>
+      </div>
+    </div>
+  );
+}
